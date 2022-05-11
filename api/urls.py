5b7a951054ff1b11c114412f137e27cd32d85fa1
ars-4 <ars7.dev@gmail.com>
2022-05-11 14:43:27 +0500

    Api

---
+++ api/urls.py
@@ -0,0 +1,21 @@
+from api import views
+from django.urls import path, include
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register('person', views.PersonViewSet)
+
+
+urlpatterns = [
+
+    path('', include(router.urls)),
+
+    path('auth/register/', views.RegisterView.as_view()),
+    path('auth/login/', views.LoginView.as_view()),
+
+    path('token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', views.TokenVerifyView.as_view(), name='token_verify'),
+
+]
